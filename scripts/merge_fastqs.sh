3a3620993f6e3c2a396731f33f695af88d5908e4
Ana Sartorius Yáñez <sartoriusana@gmail.com>
2024-01-13 17:46:19 +0100

    Merging of files with same ID into one single file.

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,22 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+
+input_dir="$1"
+
+output_dir="$2"
+
+sample_id="$3"
+
+output_file="${output_dir}/${sample_id}.fastq.gz"
+
+echo "Ficheros de entrada:"
+ls -l "${input_dir}/${sample_id}.5dpp.1.1s_sRNA.fastq" "${input_dir}/${sample_id}.5dpp.1.2s_sRNA.fastq"
+
+echo "Fichero de salida:"
+echo "${output_file}"
+
+cat "${input_dir}/${sample_id}.5dpp.1.1s_sRNA.fastq" "${input_dir}/${sample_id}.5dpp.1.2s_sRNA.fastq" > "${output_file}"
+
+echo "Files merged into ${output_file}"
